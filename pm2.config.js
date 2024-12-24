ab9d954ba8b66624af38142041d1b6245d74eaec
herveee <henzy80@hotmail.com>
2024-12-24 07:04:40 +0000

    set ignore

--- pm2.config.js
+++ pm2.config.js
@@ -1,21 +1,21 @@
-PM2 Config for Next.js
-
 module.exports = {
-  apps: [{
-    name: 'xero-connect',
-    script: 'npm',
-    args: 'start',
-    instances: 'max',
-    exec_mode: 'cluster',
-    watch: false,
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000
+  apps: [
+    {
+      name: 'xero-connect',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start',
+      instances: 'max', // Utilizes all CPU cores
+      exec_mode: 'cluster',
+      watch: false, // Avoid restart on file changes
+      env: {
+        NODE_ENV: 'production',
+      },
+      max_memory_restart: '1G',
+      error_file: 'logs/err.log',
+      out_file: 'logs/out.log',
+      merge_logs: true,
+      log_date_format: 'YYYY-MM-DD HH:mm:ss',
     },
-    max_memory_restart: '1G',
-    error_file: 'logs/err.log',
-    out_file: 'logs/out.log',
-    merge_logs: true,
-    log_date_format: 'YYYY-MM-DD HH:mm:ss'
-  }]
-}
+  ],
+};
+
